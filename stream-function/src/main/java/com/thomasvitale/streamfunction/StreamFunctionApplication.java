9c5c32cbb6df5dbdb51e96f5c479f68c7ca3d1d8
test <nazariazargul@gmail.com>
2022-05-01 19:53:46 +0200

    upgrade to latest spring boot, jdk 17

--- stream-function/src/main/java/com/thomasvitale/streamfunction/StreamFunctionApplication.java
+++ stream-function/src/main/java/com/thomasvitale/streamfunction/StreamFunctionApplication.java
@@ -1,12 +1,13 @@
 package com.thomasvitale.streamfunction;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class StreamFunctionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StreamFunctionApplication.class, args);
+		run(StreamFunctionApplication.class, args);
 	}
 }
